5e52834f98d4fd0650724c46f1c65f1ee58e8161
RaveTapp <120050958+RaveTapp@users.noreply.github.com>
2024-06-28 18:01:22 +0200

    Create files

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: './src/index.html',
+          inject: 'body',
+        }),
+    ],
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    optimization: {
+        runtimeChunk: 'single',
+    },
+};
