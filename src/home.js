ea6b7d6f13efc18d866f17e88581c71dcf2e034a
RaveTapp <120050958+RaveTapp@users.noreply.github.com>
2024-06-30 06:10:05 +0200

    Style home page

--- src/home.js
+++ src/home.js
@@ -1,21 +1,25 @@
-import restaurantImage from "./images/restaurant.jpg"
+//import restaurantImage from "./images/restaurant.jpg"
+import './home.css';
 
 export function pageLoad(){
     let content = document.querySelector("#content");
+
+    let div = document.createElement("div");
+    div.classList.add("home");
     
     const headline = document.createElement("h1");
-    headline.textContent = "The ODIN, the restaurant";
-    content.appendChild(headline);
+    headline.textContent = "The ODIN, The Restaurant";
+    div.appendChild(headline);
 
     const text = document.createElement("p");
     text.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis ratione modi dolores voluptatum! Sapiente recusandae aperiam quae impedit accusamus, saepe, aliquam veniam consectetur quas debitis blanditiis incidunt expedita quam est."
-    content.appendChild(text);
+    div.appendChild(text);
 
-    const myRestaurantImg = new Image();
+    /*const myRestaurantImg = new Image();
     myRestaurantImg.src = restaurantImage;
-    content.appendChild(myRestaurantImg);
+    content.appendChild(myRestaurantImg);*/
     
-    
+    content.appendChild(div);
 
     console.log("Home page loaded");
 }
--- webpack.config.js
+++ webpack.config.js
@@ -19,9 +19,12 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource',
-                //loader: 'file-loader',
             },
         ],
     },
